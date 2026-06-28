91102d4a1ddcc20ea04630a493229dd38497cfc3
ygyg17 <yogisuryawan0000@gmail.com>
2026-06-28 18:59:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 const CONFIG = {
   GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwJWrSQiv6gPa2Gax9rzXRBZn57uxjUTqjf1UvFNa-7wzGmlwfCJuZ8cuDtBk3X68navQ/exec"
 };
-
-window.API_URL = "https://script.google.com/macros/s/AKfycbz_AlZTjEjfpabEsRxLB_DDGOH6hUQMBYegGv1aakS_Zz4qlqICqe4nHdfWugJ5HQLWEw/exec";
