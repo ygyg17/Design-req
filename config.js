011e01d68ebecf21c1ab6f667c5f0c87f7d421de
ygyg17 <yogisuryawan0000@gmail.com>
2026-07-05 07:20:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
-const CONFIG = {
-  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwJWrSQiv6gPa2Gax9rzXRBZn57uxjUTqjf1UvFNa-7wzGmlwfCJuZ8cuDtBk3X68navQ/exec"
-};
-
-
 const SUPABASE_CONFIG = {
   URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co',
   ANON_KEY: 'sb_publishable_pT0S-ws6hJKRI7ptL7uhAg_A_GtFdFf',
   TASKS_API_URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co/functions/v1/tasks-api',
+  DESIGN_API_URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co/functions/v1/design-request-api', // BARU
   LOGIN_PAGE: 'login.html'
 };
+
+window.SUPABASE_CONFIG = SUPABASE_CONFIG;
