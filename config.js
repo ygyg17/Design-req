ce05264d2e4bd8c047099e76b1632031247c95a2
ygyg17 <yogisuryawan0000@gmail.com>
2026-07-05 19:41:36 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -2,8 +2,7 @@ const SUPABASE_CONFIG = {
   URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co',
   ANON_KEY: 'sb_publishable_pT0S-ws6hJKRI7ptL7uhAg_A_GtFdFf',
   TASKS_API_URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co/functions/v1/tasks-api',
-  DESIGN_API_URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co/functions/v1/design-request-api', // BARU
-  LOGIN_PAGE: 'login.html'
+  DESIGN_API_URL: 'https://guobvhghlrlgcqbfgzdn.supabase.co/functions/v1/design-request-api'
 };
 
 window.SUPABASE_CONFIG = SUPABASE_CONFIG;
